69675191fea1363cc86464f399d1b117c00eaf2c
Sertk <Sertk@LAPTOP-712601FB>
2020-08-24 19:28:51 +0300

    upload

---
+++ giriş/src/giriş/s.java
@@ -0,0 +1,14 @@
+package giriþ;
+import java.util.Scanner;
+
+public class s {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		Scanner scan=new Scanner(System.in);
+	    String b=scan.nextLine();
+	    System.out.println("Yazý: "+b);
+	}
+
+}
